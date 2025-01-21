fdd28f78e97e49e2d8fdf5308253d00e5f9e43a7
dphu8482 <dphu8482@uni.sydney.edu.au>
2025-01-21 18:37:59 +1100

    ignore errors

--- collector-mail/next.config.js
+++ collector-mail/next.config.js
@@ -5,6 +5,13 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    typescripts: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    }
+};
 
 export default config;
